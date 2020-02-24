27eb2875a86a59bf1a26524988d0a3bcc658cd53
Nicholas Brutti <nicholassbrutti@gmail.com>
2020-02-24 20:48:01 -0300

    Morgan e mensagem de erro

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from './routes';
+import morgan from 'morgan';
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan('dev'));
 app.use('/', routes);
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 export default app;
