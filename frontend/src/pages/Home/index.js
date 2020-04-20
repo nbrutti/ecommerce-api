6f9f5acffb432b628064de133cb332cdf21188f3
Nicholas Brutti <nicholassbrutti@gmail.com>
2020-04-19 22:32:42 -0300

    Requisição GET para listar os produtos

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiLogIn, FiPlus } from 'react-icons/fi';
 import Footer from '../../partials/Footer';
 import Header from '../../partials/Header';
+import api from '../../services/api';
 import './styles.css';
 
 function Home() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    api.get('produtos').then(response => {
+      setProdutos(response.data);
+    }).catch(err => {
+      console.log(`Não foi possível recuperar os produtos. Erro: ${err}`);
+    });
+  }, []);
+
   return (
     <div>
       <Header />
+      
       <section className="banner">
         <div className="content">
           <h1>Cadastre seus produtos agora mesmo!</h1>
@@ -28,8 +40,19 @@ function Home() {
 
       <div className="content-produtos">
         <h1>Produtos disponíveis:</h1>
+
+        <div className="cards">
+          {produtos.map(produto =>
+            <div key={produto.id} className="card">
+              <div className="container">
+                <p>Produto: {produto.nome}</p>
+                <p>Preço: {produto.preco}</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      
+
       <Footer />
     </div>
   );
